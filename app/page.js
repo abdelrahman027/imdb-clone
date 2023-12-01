3acc82fb4562df3339f787fef823d6603bfdf98e
Abdelrahman <abdelrahmansaad027@gmail.com>
2023-12-01 23:27:54 +0200

    fetch data from tmdb

--- app/page.js
+++ app/page.js
@@ -1,9 +1,27 @@
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/day"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 1000 } }
+  );
+
+  if (!res.ok)
+  {
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
 
 
-export default function Home() {
   return (
     <main>
-      Home
+      <Results results={results} />
     </main>
   )
 }
